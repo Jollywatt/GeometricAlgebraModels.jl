6ce619d193cf42dced21af7252661c168b113367
jollywatt <jo.alex.w@gmail.com>
2026-06-30 14:23:14 +0100

    plot Dupin cyclides

--- ext/liesphere.jl
+++ ext/liesphere.jl
@@ -14,3 +14,26 @@ function encode(A::TangentBlade{Lorentzian{3}, 0})
   p, r = spacetimesplit(A.p)
   encode(OrientedSphere(p, r))
 end
+
+function encode(A::RoundBlade{Lorentzian{3}, 3})
+  p, t = spacetimesplit(A.p)
+  v0 = timevector()
+
+  perp = embed(Val(3), inner(A.E, v0))
+  n = inner(inv(perp), embed(Val(3), inner(wedge(A.E, v0), v0)))
+
+  c2 = scalar_prod(n, n) - 1
+  if c2 >= 0
+    Rig(
+      "Dupin Cyclide",
+      location=p,
+      "x axis"=>n,
+      "y axis"=>perp,
+      "b"=>√A.r2,
+      "c"=>-1/√c2,
+      "d"=>t,
+    )
+  else
+    encode(opns(Multivector(A)))
+  end
+end
